b09e4c1d752244a7e8d07e393c2f0ef1a6a7b362
Qun Li Huang <qunli02@gmail.com>
2019-04-03 20:00:45 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new = []
+  while i < collectio.length
+    if yield collection[i]  == true
+      new.push(collection[i])
+    end
+    i += 1
+  end
+ new
 end
