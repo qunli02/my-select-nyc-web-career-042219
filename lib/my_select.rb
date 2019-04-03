e3ead8ea7058ec43e9a65a06ccfcb6b8d7a7d70a
Qun Li Huang <qunli02@gmail.com>
2019-04-03 20:09:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
   i = 0
   new = []
   while i < collection.length
-    yield collection[i]
+   if yield collection[i] == true
+     new = collection[i]
+   end
     i += 1
   end
  new
