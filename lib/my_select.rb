82987ad493f59daf1b130a6d4cdf81d848e7b720
Qun Li Huang <qunli02@gmail.com>
2019-04-03 20:16:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new = []
+  while i < collection.length
+   state = yield collection[i]
+    if state == true
+      new.push(collection[i])
+    end
+    i += 1
+  end
+ new
 end
+
